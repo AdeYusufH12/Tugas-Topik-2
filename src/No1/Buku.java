80767b5bf253fbb82b6c61d11989597678299a8a
Ade Yusuf Hidayat <71366389+AdeYusufH12@users.noreply.github.com>
2021-10-06 22:02:37 +0700

    Add files via upload

---
+++ src/No1/Buku.java
@@ -0,0 +1,68 @@
+package No1;
+
+public class Buku {
+    private String isbn;
+    private String judulBuku;
+    private String pengarang;
+    private String  tahunTerbit;
+    private double harga;
+    private float rating;
+
+    public Buku(String isbn, String judulBuku, String pengarang, String tahunTerbit, double harga, float rating) {
+        this.isbn = isbn;
+        this.judulBuku = judulBuku;
+        this.pengarang = pengarang;
+        this.tahunTerbit = tahunTerbit;
+        this.harga = harga;
+        this.rating = rating;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getJudulBuku() {
+        return judulBuku;
+    }
+
+    public void setJudulBuku(String judulBuku) {
+        this.judulBuku = judulBuku;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(String tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+}
